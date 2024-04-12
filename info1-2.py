789e9fc3b89799a8fc41010be181c3cf1175f7fd
Alan-Duocuc <alanbadillasandoval2091@gmail.com>
2024-04-12 00:28:38 +0000

    Archivos de la Evaluación1

---
+++ info1-2.py
@@ -0,0 +1,9 @@
+nombre = input("Ingrese el nombre del estudiante: ")
+apellido = input("Ingrese el apellido del estudiante: ")
+asignatura = input("Ingrese la asignatura del estudiante: ")
+sede = input("Ingrese la sede de Duoc del estudiante: ")
+semestre = input("Ingrese el semestre del estudiante: ")
+año = input("Ingrese el año del estudiante: ")
+
+informacion = f"{nombre} {apellido}, Asignatura: {asignatura}, Sede: {sede}, Semestre: {semestre}, Año: {año}"
+print(informacion)
